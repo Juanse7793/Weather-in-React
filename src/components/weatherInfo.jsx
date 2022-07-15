d60cc6e58b37a38bdeed99c48482e336389b418f
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-07-15 14:30:44 -0500

    info component

---
+++ src/components/weatherInfo.jsx
@@ -0,0 +1,14 @@
+const WeatherInfo = ({ weather }) => {
+    return (
+        <div>
+            <div>{weather?.location.name}</div>
+            <div>{weather?.location.country}</div>
+            <div>
+                <div>
+                    <img src= {`http:${weather?.current.condition.icon}`} width='128' alt="weather icon" />
+                </div>
+            </div>
+        </div>
+    );
+}
+export default WeatherInfo;
